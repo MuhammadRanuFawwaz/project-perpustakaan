9937cfebffd714dac986fdc695424c6add1d8eb3
MuhammadRanuFawwaz <ranufawaz95@gmail.com>
2026-05-21 16:00:30 +0700

    Fix duplicate buku columns migration

--- database/migrations/2026_05_16_184103_add_jenjang_dan_ddc_to_buku_table.php
+++ database/migrations/2026_05_16_184103_add_jenjang_dan_ddc_to_buku_table.php
@@ -9,18 +9,32 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('buku', function (Blueprint $table) {
-            $table->string('jenjang_kelas')->nullable()->after('id_kategori');
-            $table->string('kode_ddc')->nullable()->after('jenjang_kelas');
+
+            if (!Schema::hasColumn('buku', 'jenjang_kelas')) {
+                $table->string('jenjang_kelas')
+                    ->nullable()
+                    ->after('id_kategori');
+            }
+
+            if (!Schema::hasColumn('buku', 'kode_ddc')) {
+                $table->string('kode_ddc')
+                    ->nullable()
+                    ->after('jenjang_kelas');
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('buku', function (Blueprint $table) {
-            $table->dropColumn([
-                'jenjang_kelas',
-                'kode_ddc'
-            ]);
+
+            if (Schema::hasColumn('buku', 'jenjang_kelas')) {
+                $table->dropColumn('jenjang_kelas');
+            }
+
+            if (Schema::hasColumn('buku', 'kode_ddc')) {
+                $table->dropColumn('kode_ddc');
+            }
         });
     }
 };
