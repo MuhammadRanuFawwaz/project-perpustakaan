28ba83919f433e2241ccd644de99fb0b00bd8323
MuhammadRanuFawwaz <ranufawaz95@gmail.com>
2026-05-22 06:03:45 +0700

    Fix css path

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- LINK CSS -->
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="/css/login.css?v=10">
 </head>
 
 <body>
