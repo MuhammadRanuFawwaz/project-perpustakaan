ead6d1185affee7254490cacc017395d862f954c
MuhammadRanuFawwaz <ranufawaz95@gmail.com>
2026-05-22 07:07:54 +0700

    fix vite error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,8 @@
         rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 </head>
 
