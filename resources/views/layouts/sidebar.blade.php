a1d3dc23efd8a8d027c32a4380da8c1190304d47
MirzaKhair <khairmirza88@gmail.com>
2026-06-12 12:33:16 +0700

    nambah icon

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -23,41 +23,41 @@
         </a>
 
         @if (auth()->user()->role === 'superadmin')
-        <a href="{{ route('master.murid.index') }}"
-            class="{{ request()->routeIs('master.murid.*') ? 'active' : '' }}">
-            <img src="{{ asset('icon/murid.png') }}" class="menu-icon">
-            Data Murid
-        </a>
+            <a href="{{ route('master.murid.index') }}"
+                class="{{ request()->routeIs('master.murid.*') ? 'active' : '' }}">
+                <img src="{{ asset('icon/murid.png') }}" class="menu-icon">
+                Data Murid
+            </a>
 
-        <a href="{{ route('master.guru.index') }}"
-            class="{{ request()->routeIs('master.guru.*') ? 'active' : '' }}">
-            <img src="{{ asset('icon/guru.png') }}" class="menu-icon">
-            Data Guru
-        </a>
+            <a href="{{ route('master.guru.index') }}"
+                class="{{ request()->routeIs('master.guru.*') ? 'active' : '' }}">
+                <img src="{{ asset('icon/guru.png') }}" class="menu-icon">
+                Data Guru
+            </a>
 
-        <a href="{{ route('master.kategori.index') }}"
-            class="{{ request()->routeIs('master.kategori.*') ? 'active' : '' }}">
-            <img src="{{ asset('icon/kategori.png') }}" class="menu-icon">
-            Master Kategori
-        </a>
+            <a href="{{ route('master.kategori.index') }}"
+                class="{{ request()->routeIs('master.kategori.*') ? 'active' : '' }}">
+                <img src="{{ asset('icon/kategori.png') }}" class="menu-icon">
+                Master Kategori
+            </a>
 
-        <a href="{{ route('master.ddc.index') }}"
-            class="{{ request()->routeIs('master.ddc.*') ? 'active' : '' }}">
-            <img src="{{ asset('icon/ddc.png') }}" class="menu-icon">
-            Master DDC
-        </a>
+            <a href="{{ route('master.ddc.index') }}"
+                class="{{ request()->routeIs('master.ddc.*') ? 'active' : '' }}">
+                <img src="{{ asset('icon/ddc.png') }}" class="menu-icon">
+                Master DDC
+            </a>
 
-        <a href="{{ route('master.harga-buku.index') }}"
-            class="{{ request()->routeIs('master.harga-buku.*') ? 'active' : '' }}">
-            <img src="{{ asset('icon/harga.png') }}" class="menu-icon">
-            Master Harga Buku
-        </a>
+            <a href="{{ route('master.harga-buku.index') }}"
+                class="{{ request()->routeIs('master.harga-buku.*') ? 'active' : '' }}">
+                <img src="{{ asset('icon/harga.png') }}" class="menu-icon">
+                Master Harga Buku
+            </a>
 
-        <a href="{{ route('master.akses-admin.index') }}"
-            class="{{ request()->routeIs('master.akses-admin.*') ? 'active' : '' }}">
-            <img src="{{ asset('icon/profile.png') }}" class="menu-icon">
-            Hak Akses Admin
-        </a>
+            <a href="{{ route('master.akses-admin.index') }}"
+                class="{{ request()->routeIs('master.akses-admin.*') ? 'active' : '' }}">
+                <img src="{{ asset('icon/akses.png') }}" class="menu-icon">
+                Hak Akses Admin
+            </a>
         @endif
 
         <a href="{{ route('pengunjung.index') }}" class="{{ request()->routeIs('pengunjung.*') ? 'active' : '' }}">
